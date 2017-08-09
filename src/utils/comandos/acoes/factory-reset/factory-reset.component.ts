e231d73f82427b09dc0df202012e8af91f08f185
FabioClem <fabio.henrique.clem@gmail.com>
2017-08-09 09:02:04 -0300

    Adicionado componente Factory Reset (Testar) *koalization*

--- src/utils/comandos/acoes/factory-reset/factory-reset.component.ts
+++ src/utils/comandos/acoes/factory-reset/factory-reset.component.ts
@@ -1,3 +1,4 @@
+import { HolderService } from './../../../holder/holder.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FactoryResetService } from './factory-reset.service';
 import { Component, OnInit } from '@angular/core';
@@ -11,14 +12,42 @@ import { Component, OnInit } from '@angular/core';
 
 export class FactoryResetComponent implements OnInit {
 
+    private nomeBtn: string = "Sim";
+    private disableBtn: boolean = false;
+
     constructor(
-        public activeModal: NgbActiveModal) { }
+        public activeModal: NgbActiveModal,
+        private factoryResetService: FactoryResetService,
+        public holderService: HolderService) { }
 
     ngOnInit() { }
 
     factoryReset() {
-        console.log("Facotry Reset Fake");
+        if (!this.disableBtn) {
+            this.disableBtn = true;
+            this.nomeBtn = "Aguarde";
+            this.factoryResetService.factoryReset(this.holderService.equipamento)
+                .then(data => {
+                    if (data) {
+                        this.callAlert("Reset de fábrica realizado com sucesso, aguarde as configurações do modem.", "success");
+                        this.activeModal.close()
+                        this.holderService.checkOnline = false;
+                    } else {
+                        this.callAlert("Reset de fábrica não realizado.", "danger");
+                    }
+                }, error => {
+                    this.callAlert("problema ao realizar reset de fábrica no modem.", "danger");
+                });
 
+        }
+    }
+
+    callAlert(msg, type) {
+        this.holderService.alertOn = true;
+        this.holderService.alertInfo = {
+            alertType: type,
+            alertMsg: msg
+        }
     }
 
 }
--- src/utils/comandos/acoes/factory-reset/factory-reset.service.ts
+++ src/utils/comandos/acoes/factory-reset/factory-reset.service.ts
@@ -1,8 +1,48 @@
+import { Equipamento } from './../../../../viewmodel/equipamento/equipamento';
+import { Headers, RequestOptions, Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FactoryResetService {
 
-    constructor() { }
-    
+    private headersAppJson = new Headers({ 'Content-Type': 'application/json' });
+    private options = new RequestOptions({ headers: this.headersAppJson });
+    private url = "http://10.40.195.81:8080/acs/";
+
+    constructor(
+        private http: Http) { }
+
+    factoryReset(device: Equipamento): Promise<Boolean> {
+        let usr = JSON.parse(sessionStorage.getItem('user'));
+        const url = `${this.url}` + "device/reboot";
+        let _data: { device: Equipamento, executor: string };
+        _data = { device: device, executor: usr.usr }
+        return this.http.post(url, JSON.stringify(_data), this.options)
+            .timeout(120000)
+            .toPromise()
+            .then(response => {
+                return response.json() as Boolean
+            })
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        //console.error('Ocorreu o seguinte erro: ', error); // for demo purposes only
+        let er: any;
+        if (error.message === "Timeout has occurred") {
+            er = {
+                tError: "Timeout",
+                mError: "Tempo de busca excedido, por favor realize a busca novamente, caso o problema persista informe ao administrador do sistema."
+            }
+        } else {
+            let erJson: any;
+            erJson = error.json();
+            er = {
+                tError: "",
+                mError: erJson.message
+            }
+        }
+        return Promise.reject(er);
+    }
+
 }
