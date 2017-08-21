79da136022974afc3ea06942387c1e3bc9d3356d
FabioClem <fabio.henrique.clem@gmail.com>
2017-08-21 17:26:44 -0300

    Apontado para produção *Koalization*

--- src/utils/url-service/url.service.ts
+++ src/utils/url-service/url.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class UrlService {
 
-    private urlIp = "10.40.195.81"; // Produção / 10.200.35.67
+    private urlIp = "10.200.35.66"; // Produção / 10.200.35.67
     //private urlIp = "localhost"; // Dev 
 
     private headersAppJson = new Headers({ 'Content-Type': 'application/json' });
